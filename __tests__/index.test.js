7a192dd923147c60ebbe75258c8712e2379548b6
Andrés Quilodrán <andres.caro.q@gmail.com>
2021-08-05 18:05:48 -0400

    Add Jest and trivial App level component test

---
+++ __tests__/index.test.js
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+import App from '../pages/index';
+
+const mainHeading = 'Esquad Icon of person performing a squat';
+
+describe('App', () => {
+  it('render without crashing', () => {
+    render(<App />);
+    expect(
+      screen.getByRole('heading', { name: mainHeading }),
+    ).toBeInTheDocument();
+  });
+});
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import React, { useState } from 'react';
+import { useState } from 'react';
 import styles from '../styles/Home.module.css';
 
 import TeamsForm from '../components/teamsForm';

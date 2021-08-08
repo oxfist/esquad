1822d4878366e7b87c7babb897439161c5537ac0
Andrés Quilodrán <andres.caro.q@gmail.com>
2021-08-08 14:04:26 -0400

    Rename component files for better clarity of what is a component

--- __tests__/components/index.test.jsx
+++ __tests__/components/index.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from '../pages/index';
+import App from '../../pages/index';
 
 const mainHeading = 'Esquad Icon of person performing a squat';
 

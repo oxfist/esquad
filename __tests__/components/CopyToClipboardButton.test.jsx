73c4b984459ce8907fa03d5c83058a98db33d929
Andrés Quilodrán <andres.caro.q@gmail.com>
2022-01-16 15:00:18 -0300

    chore: test stubbing of markdownizer

--- __tests__/components/CopyToClipboardButton.test.jsx
+++ __tests__/components/CopyToClipboardButton.test.jsx
@@ -9,7 +9,7 @@ describe('CopyToClipboardButton', () => {
   beforeEach(() => {
     const mockClipboard = { writeText: jest.fn() };
     global.navigator.clipboard = mockClipboard;
-    markdownizer.markdownize = jest.fn(() => '');
+    markdownizer.markdownize = jest.fn(() => console.log('asdf'));
   });
 
   afterEach(() => {
